b71a9174054ca065e199205f8294b09a4dbf8536
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-06-04 17:00:55 -0500

    reverse: :ambulance: nvim_set_option_value to nvim_win_set_option :(

--- lua/specs/init.lua
+++ lua/specs/init.lua
@@ -29,13 +29,15 @@ function M.should_show_specs(start_win_id)
   end
 
   local buftype, filetype, ok
-  ok, buftype = pcall(vim.api.nvim_get_option_value, 'buftype', 0)
+  -- ok, buftype = pcall(vim.api.nvim_get_option_value, 0, 'buftype')
+  ok, buftype = pcall(vim.api.nvim_win_get_option, 0, 'buftype')
 
   if ok and opts.ignore_buftypes[buftype] then
     return false
   end
 
-  ok, filetype = pcall(vim.api.nvim_get_option_value, 'filetype', 0)
+  -- ok, filetype = pcall(vim.api.nvim_get_option_value, 0, 'filetype')
+  ok, filetype = pcall(vim.api.nvim_win_get_option, 0, 'filetype')
 
   if ok and opts.ignore_filetypes[filetype] then
     return false
@@ -73,8 +75,10 @@ function M.show_specs(popup)
     }
   )
 
-  vim.api.nvim_set_option_value( 'winhl', { 'win', win_id }, 'Normal:'.. _opts.popup.winhl )
-  vim.api.nvim_set_option_value( 'winblend', { "win", win_id }, _opts.popup.blend )
+  vim.api.nvim_win_set_option(win_id, 'winhl', 'Normal:'.. _opts.popup.winhl)
+  -- vim.api.nvim_set_option_value( 'winhl', { 'win', win_id }, 'Normal:'.. _opts.popup.winhl )
+  vim.api.nvim_win_set_option(win_id, "winblend", _opts.popup.blend)
+  -- vim.api.nvim_set_option_value( 'winblend', { "win", win_id }, _opts.popup.blend )
 
   local cnt = 0
   local config = vim.api.nvim_win_get_config(win_id)
@@ -100,7 +104,8 @@ function M.show_specs(popup)
       local dm = _opts.popup.resizer(_opts.popup.width, cursor_col, cnt)
 
       if bl ~= nil then
-        vim.api.nvim_win_set_option_value(win_id, { winblend = bl })
+        -- vim.api.nvim_set_option_value('winblend', win_id, { winblend = bl })
+        vim.api.nvim_win_set_option(win_id, "winblend", bl)
       end
       if dm ~= nil then
         config.col = dm[2]
